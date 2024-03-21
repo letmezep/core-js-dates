61f5cad51ca3e76d8ec81e9741325f32fcea9d92
letmezep <letmezep@gmail.com>
2024-03-21 13:54:52 +0800

    3 passing 10 pending 1 failing

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -17,8 +17,9 @@
  * '01 Jan 1970 00:00:00 UTC' => 0
  * '04 Dec 1995 00:12:00 UTC' => 818035920000
  */
-function dateToTimestamp(/* date */) {
-  throw new Error('Not implemented');
+function dateToTimestamp(date) {
+  const timestamp = new Date(date).getTime();
+  return timestamp;
 }
 
 /**
@@ -31,8 +32,11 @@ function dateToTimestamp(/* date */) {
  * Date(2023, 5, 1, 8, 20, 55) => '08:20:55'
  * Date(2015, 10, 20, 23, 15, 1) => '23:15:01'
  */
-function getTime(/* date */) {
-  throw new Error('Not implemented');
+function getTime(date) {
+  const hours = date.getHours().toString().padStart(2, '0');
+  const minutes = date.getMinutes().toString().padStart(2, '0');
+  const seconds = date.getSeconds().toString().padStart(2, '0');
+  return `${hours}:${minutes}:${seconds}`;
 }
 
 /**
@@ -46,8 +50,19 @@ function getTime(/* date */) {
  * '03 Dec 1995 00:12:00 UTC' => 'Sunday'
  * '2024-01-30T00:00:00.000Z' => 'Tuesday'
  */
-function getDayName(/* date */) {
-  throw new Error('Not implemented');
+function getDayName(date) {
+  const daysOfWeek = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+  ];
+  const newdate = new Date(date);
+  const dayIndex = newdate.getUTCDay();
+  return daysOfWeek[dayIndex];
 }
 
 /**
@@ -61,8 +76,15 @@ function getDayName(/* date */) {
  * Date('2024-02-13T00:00:00Z') => Date('2024-02-16T00:00:00Z')
  * Date('2024-02-16T00:00:00Z') => Date('2024-02-23T00:00:00Z')
  */
-function getNextFriday(/* date */) {
-  throw new Error('Not implemented');
+function getNextFriday(date) {
+  const newdate = new Date(date);
+  const dayOfWeek = newdate.getDay();
+  const daysUntilFriday = (5 - dayOfWeek + 7) % 7; // Calculate days until next Friday
+  const nextFriday = new Date(
+    newdate.getTime() + daysUntilFriday * 24 * 60 * 60 * 1000
+  );
+  nextFriday.setHours(0, 0, 0, 0);
+  return nextFriday;
 }
 
 /**
