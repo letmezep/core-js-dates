cea9c4d18e6b3ff3b95c3b7fb0363d70d39b5fca
letmezep <letmezep@gmail.com>
2024-03-21 23:32:56 +0800

    11 passing 3 pending

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -248,8 +248,14 @@ function getWeekNumberByDate(date) {
  * Date(2024, 0, 13) => Date(2024, 8, 13)
  * Date(2023, 1, 1) => Date(2023, 9, 13)
  */
-function getNextFridayThe13th(/* date */) {
-  throw new Error('Not implemented');
+function getNextFridayThe13th(date) {
+  const friday = 5;
+  while (true) {
+    date.setDate(date.getDate() + 1);
+    if (date.getDate() === 13 && date.getDay() === friday) {
+      return date;
+    }
+  }
 }
 
 /**
